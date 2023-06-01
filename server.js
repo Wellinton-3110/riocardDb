38aa8a1f4c986351365fc8015d85637207a29c1e
Wellinton <wellintongerson@hotmail.com>
2023-06-01 00:03:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/usuarios", async (req, res) => {
+  const data = await prisma.user.findMany();
+  return res.json(data);
+});
+
+app.post("/usuarios", async (req, res) => {
+  const body = req.body;
+  const user = await prisma.user.create({
+    data: {
+      nome: body.nome,
+      email: body.email,
+      login: body.login,
+      senha: body.senha,
+    },
+  });
+  return res.json(user);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  const userID = Number(req.params.id);
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: userID,
+    },
+  });
+  return res.json(deleteUser);
+});
+
+app.listen(port);
