aba88e3a5f0f58538d1f93ed46048884e1c160e7
Wellinton <wellintongerson@hotmail.com>
2023-06-01 00:28:38 -0300

    add: database file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const prisma = new PrismaClient({
 
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
